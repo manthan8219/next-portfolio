a4a13f6f7eb17f999b36d6828eec8220e94ee758
manthan <manthanbhatia367@gmail.com>
2022-06-24 10:24:29 +0530

    updated

--- components/Projects.js
+++ components/Projects.js
@@ -6,6 +6,7 @@ import orm from '../public/assests/orm.jpg'
 import node from '../public/assests/node.png'
 import Reactjs from "../public/assests/ReactJs.png"
 import boot from "../public/assests/boot.png"
+import hulu from "../public/assests/hulu.jpg"
 
 const Projects = () => {
   return (
@@ -16,12 +17,14 @@ const Projects = () => {
         </p>
         <h2 className='py-4'>What I&apos;ve Built</h2>
         <div className='grid md:grid-cols-2 gap-8'>
+        <ProjectItems title='Hulu-clone' backgroundImage={hulu} URL='/Hulu' technology="Next.js" />
          <ProjectItems title='Crud application' backgroundImage={angular} URL='/Angular' technology="Angular" />
          <ProjectItems title='Crud application' backgroundImage={node} URL='/Node' technology="Node" />
          <ProjectItems title='Crud application + Api' backgroundImage={boot} URL='/Spring_boot' technology="Springboot" />
          <ProjectItems title='Portfolio' backgroundImage={Reactjs} URL='/ReactProject' technology="React js" />
          <ProjectItems title='Database connection' backgroundImage={orm} URL='/Database' technology="Java" />
          <ProjectItems title='Data Structures' backgroundImage={data} URL='/Data_Structures' technology="Java" />
+        
         </div>
       </div>
     </div>
---
+++ pages/Hulu.js
@@ -0,0 +1,73 @@
+import React from "react";
+import Link from "next/link";
+import { RiRadioButtonFill } from "react-icons/ri";
+import Image from "next/image";
+import hulu from "../public/assests/hulu.jpg";
+const Hulu = () => {
+  return (
+    <div className="w-full">
+      <div className="w-screen h-[30vh] lg:h-[40vh] relative">
+        <div className="absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/80 z-10" />
+        <Image
+          className="absolute z-1"
+          layout="fill"
+          objectFit="cover"
+          src={hulu}
+          alt="/"
+        />
+        <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2">
+          <h2 className="py-2">Hulu-Clone</h2>
+          <h3>Node.js </h3>
+        </div>
+      </div>
+
+      <div className="max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 pt-8">
+        <div className="col-span-4">
+          <p>Project</p>
+          <h2>Overview</h2>
+          <p>
+            This project is a clone of hulu where we can see the top rated of
+            the top action movies or any kind of genre which is present with
+            some slight animation it is just front page made with react,nextjs and tailwind css 
+          </p>
+          <a
+            href="https://hulu-clone-seven-delta.vercel.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="px-8 py-2 mt-4 mr-8">Demo</button>
+          </a>
+          <a
+            href="https://github.com/manthan8219/hulu-clone"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <button className="px-8 py-2 mt-4">Code</button>
+          </a>
+        </div>
+        <div className="col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl p-4">
+          <div className="p-2">
+            <p className="text-center font-bold pb-2">Technologies</p>
+            <div className="grid grid-cols-3 md:grid-cols-1">
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> React
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" />
+                Next js
+              </p>
+              <p className="text-gray-600 py-2 flex items-center">
+                <RiRadioButtonFill className="pr-1" /> Tailwindcss
+              </p>
+            </div>
+          </div>
+        </div>
+        <Link href="/#projects">
+          <p className="underline cursor-pointer">Back</p>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Hulu;
